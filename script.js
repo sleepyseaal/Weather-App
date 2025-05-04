7294110e837b4e8a541e1a968a9be2944472bca1
mohamed <sickmido@gmail.com>
2025-05-04 12:03:57 +0300

    Fetch city summary data

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-async function getData(cityName) {
+async function getWeatherData(cityName) {
   try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=8982eac9681b6860f252c3f99695a774&units=metric`
-    );
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=8982eac9681b6860f252c3f99695a774&units=metric`;
+
+    const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
+  }
+}
+
+async function getCityData(cityName) {
+  try {
+    const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${cityName}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
   }
 }
