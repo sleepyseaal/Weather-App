6cda1c3d69ae34db9bb0c49836efc4f52db4e303
mohamed <sickmido@gmail.com>
2025-05-05 09:42:43 +0300

    Display weather degree and description
    Add a function to categorize the weather state based on fetched degree, update displayWeatherFunction to display the weather's informations(degree, state and description)

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const userInput = document.getElementById("city-input");
 const searchButton = document.getElementById("search-button");
 
+const tempCard = document.querySelector(".temp");
 const cityNameCard = document.querySelector(".city");
 const cityDataCard = document.querySelector(".city-data");
+
+const weatherStateCard = document.querySelector(".weather-state1");
+const weatherDescriptionCard = document.querySelector(".weather-state2");
 
 async function getWeatherData(cityName) {
   try {
@@ -29,12 +33,59 @@ async function getCityData(cityName) {
 }
 
 function displayWeatherData(cityName) {
-  getWeatherData(cityName).then((data) => {});
+  getWeatherData(cityName).then((data) => {
+    const degree = data.list[0].main.temp;
+
+    // Display temp degree
+    tempCard.textContent = `${degree}°`;
+
+    // Get weather state based on temp degree
+    function getTemperatureState(temp) {
+      let category;
+
+      // Categorizing weather state
+      if (temp <= 0) {
+        category = "freezing";
+      } else if (temp <= 10) {
+        category = "cold";
+      } else if (temp <= 20) {
+        category = "mild";
+      } else if (temp <= 30) {
+        category = "warm";
+      } else {
+        category = "hot";
+      }
+
+      switch (category) {
+        case "freezing":
+          return "Freezing ❄️";
+        case "cold":
+          return "Cold 🧥";
+        case "mild":
+          return "Mild 🌤️";
+        case "warm":
+          return "Warm ☀️";
+        case "hot":
+          return "Hot 🔥";
+        default:
+          return "Unknown";
+      }
+    }
+
+    const weatherState = getTemperatureState(degree);
+    weatherStateCard.textContent = weatherState;
+
+    // Display weather description
+    weatherDescriptionCard.textContent = `With ${data.list[0].weather[0].description}`;
+  });
 }
 
 function displayCityData(cityName) {
   getCityData(cityName).then((data) => {
+    // Display city's name
     cityNameCard.textContent = `${data.title}`;
+
+    // display informations about the city
     cityDataCard.textContent = `${data.extract}`;
   });
 }
@@ -42,4 +93,5 @@ function displayCityData(cityName) {
 searchButton.addEventListener("click", () => {
   let city = userInput.value;
   displayCityData(city);
+  displayWeatherData(city);
 });
